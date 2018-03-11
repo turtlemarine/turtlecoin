12b3082326c9c649134218e458d7dd1bf603671e
turtlemarine <37258101+turtlemarine@users.noreply.github.com>
2018-03-11 01:27:25 -0500

    Update easy_installer.sh

--- easy_installer.sh
+++ easy_installer.sh
@@ -6,7 +6,7 @@
 sudo apt-get update
 yes "" | sudo apt-get install build-essential python-dev gcc-4.9 g++-4.9 git cmake libboost1.58-all-dev librocksdb-dev
 export CXXFLAGS="-std=gnu++11"
-git clone https://github.com/turtlemarinecoin/turtlemarinecoin
+git clone https://github.com/turtlemarine/turtlemarinecoin
 cd turtlemarinecoin
 mkdir build && cd $_
 cmake ..
